d876008fa027ea28e151cc2b93c33e33a44b11dc
Jack Huang <jackhxs@gmail.com>
2024-01-21 15:59:10 -0800

    fix(018): fix winding order and start/end connection

--- day-018-lavaduct-lagoon/src/lib.rs
+++ day-018-lavaduct-lagoon/src/lib.rs
@@ -60,7 +60,7 @@ pub struct LavaductLagoon {
 }
 
 impl LavaductLagoon {
-    fn hex_area(&self) -> isize {
+    fn hex_area(&self) -> usize {
         let mut prev_point = Coordinate::from((0_isize, 0_isize));
         let mut prev_dir = self.plans[self.plans.len() - 1].hex_dir;
         let mut area = 0;
@@ -71,8 +71,12 @@ impl LavaductLagoon {
         for plan in &self.plans {
             if prev_dir.right() == plan.hex_dir {
                 right_turns += 1;
-            } else {
+            } else if prev_dir.left() == plan.hex_dir {
                 left_turns += 1;
+            } else {
+                // first plan and last plan form a straight side, not a corner
+                // in this case, we add 1 more to the perimeter
+                perimeter += 1;
             }
 
             let p = prev_point.steps(&plan.hex_dir, plan.hex_length);
@@ -83,10 +87,15 @@ impl LavaductLagoon {
             prev_point = p;
         }
 
-        area.abs() / 2 + perimeter as isize / 2 + (right_turns * 3 + left_turns) / 4
+        // positive means counterclockwise winding, negative means clockwise winding
+        if area > 0 {
+            (area as usize * 2 + perimeter * 2 + left_turns * 3 + right_turns) / 4
+        } else {
+            (-area as usize * 2 + perimeter * 2 + right_turns * 3 + left_turns) / 4
+        }
     }
 
-    fn area(&self) -> isize {
+    fn area(&self) -> usize {
         let mut prev_point = Coordinate::from((0_isize, 0_isize));
         let mut prev_dir = self.plans[self.plans.len() - 1].dir;
         let mut area = 0;
@@ -97,8 +106,12 @@ impl LavaductLagoon {
         for plan in &self.plans {
             if prev_dir.right() == plan.dir {
                 right_turns += 1;
-            } else {
+            } else if prev_dir.left() == plan.dir {
                 left_turns += 1;
+            } else {
+                // first plan and last plan form a straight side, not a corner
+                // in this case, we add 1 more to the perimeter
+                perimeter += 1;
             }
 
             let p = prev_point.steps(&plan.dir, plan.length);
@@ -109,7 +122,12 @@ impl LavaductLagoon {
             prev_point = p;
         }
 
-        area.abs() / 2 + perimeter as isize / 2 + (right_turns * 3 + left_turns) / 4
+        // positive means counterclockwise winding, negative means clockwise winding
+        if area > 0 {
+            (area as usize * 2 + perimeter * 2 + left_turns * 3 + right_turns) / 4
+        } else {
+            (-area as usize * 2 + perimeter * 2 + right_turns * 3 + left_turns) / 4
+        }
     }
 }
 
@@ -131,8 +149,8 @@ impl Problem for LavaductLagoon {
     const README: &'static str = include_str!("../README.md");
 
     type ProblemError = anyhow::Error;
-    type P1 = isize;
-    type P2 = isize;
+    type P1 = usize;
+    type P2 = usize;
 
     fn part_one(&mut self) -> Result<Self::P1, Self::ProblemError> {
         Ok(self.area())
